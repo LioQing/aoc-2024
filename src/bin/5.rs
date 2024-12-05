e361d99c2be900bfea15c0a210cc903f0c670316
LioQing <s2010157@gmail.com>
2024-12-05 14:02:29 +0800

    Add day 5

---
+++ src/bin/5.rs
@@ -0,0 +1,71 @@
+use std::collections::HashSet;
+
+use aoc_2024::input;
+use itertools::Itertools;
+
+fn parse_input(input: &str) -> (HashSet<(i32, i32)>, Vec<Vec<i32>>) {
+    fn parse_rules(input: &str) -> HashSet<(i32, i32)> {
+        input
+            .lines()
+            .map(|line| line.split_once("|").unwrap())
+            .map(|(a, b)| (a.parse().unwrap(), b.parse().unwrap()))
+            .collect()
+    }
+
+    fn parse_orders(input: &str) -> Vec<Vec<i32>> {
+        input
+            .lines()
+            .map(|line| line.split(",").map(|n| n.parse().unwrap()).collect_vec())
+            .collect_vec()
+    }
+
+    let (rules, orders) = input.split_once("\n\n").unwrap();
+    (parse_rules(rules), parse_orders(orders))
+}
+
+fn solution_part_1(rules: &HashSet<(i32, i32)>, orders: &[Vec<i32>]) -> i32 {
+    orders
+        .iter()
+        .filter(|order| {
+            order
+                .iter()
+                .copied()
+                .tuple_combinations()
+                .all(|(a, b)| rules.contains(&(a, b)))
+        })
+        .map(|order| order.get(order.len() / 2).unwrap())
+        .sum()
+}
+
+fn solution_part_2(rules: &HashSet<(i32, i32)>, orders: &[Vec<i32>]) -> i32 {
+    orders
+        .iter()
+        .filter(|order| {
+            order
+                .iter()
+                .copied()
+                .tuple_combinations()
+                .any(|(a, b)| !rules.contains(&(a, b)))
+        })
+        .map(|order| {
+            order
+                .iter()
+                .copied()
+                .sorted_by(|a, b| match rules.contains(&(*a, *b)) {
+                    true => std::cmp::Ordering::Less,
+                    _ => std::cmp::Ordering::Greater,
+                })
+                .collect_vec()
+        })
+        .map(|order| *order.get(order.len() / 2).unwrap_or(&0))
+        .sum()
+}
+
+fn main() {
+    let input = input(5);
+
+    let (rules, orders) = parse_input(&input);
+
+    println!("Part 1 solution: {}", solution_part_1(&rules, &orders));
+    println!("Part 2 solution: {}", solution_part_2(&rules, &orders));
+}
